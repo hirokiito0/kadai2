35f479c738698a6256a2fe205dc206848880417c
EC2 Default User <ec2-user@ip-172-31-84-123.ec2.internal>
2021-06-07 14:13:59 +0000

    [update]4-8_kadai2_新規登録機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book=Book.new
   end
 
   def show
@@ -10,10 +11,17 @@ class BooksController < ApplicationController
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
   end
 
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
 end
