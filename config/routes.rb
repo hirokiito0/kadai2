a299cbb3c70ce7b042f090153911a568293d48fc
EC2 Default User <ec2-user@ip-172-31-84-123.ec2.internal>
2021-06-08 01:41:26 +0000

    [update]4-8_kadai2_indexページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'top' => 'homes#top'
   resources :books
-  
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
