c3af94bf0483c3c5f6ff514de9a3c43081772a06
EC2 Default User <ec2-user@ip-172-31-84-123.ec2.internal>
2021-06-08 06:32:17 +0000

    [update]4-8_kadai2_flashメッセージupdate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,8 +33,13 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.save
+      @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = 'Book was successfully updated.'
+    else
+      redirect_to book_path(@book.id)
+    end
   end
 
   private
